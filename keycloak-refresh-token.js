19133770686ff0e8e638bc6cef40e6c997aebfa2
Robert <11veigar@gmail.com>
2019-08-21 10:00:49 +0200

    Update keycloak-refresh-token.js

--- keycloak-refresh-token.js
+++ keycloak-refresh-token.js
@@ -1,6 +1,6 @@
 //refresh token script
 pm.sendRequest({
-    url: pm.environment.get('host')+"/auth/realms/"+pm.environment.get('realm')+"/protocol/openid-connect/token",
+    url: pm.environment.get('keycloakHost')+"/auth/realms/"+pm.environment.get('realm')+"/protocol/openid-connect/token",
     method: 'POST',
     header: {
        'Content-Type': "application/x-www-form-urlencoded"
@@ -8,7 +8,7 @@ pm.sendRequest({
     body: {
         mode: 'urlencoded',
         urlencoded: [
-            {key: "client_id", value: "account", disabled: false, description: {content:"", type:"text/plain"}},
+            {key: "client_id", value: pm.environment.get('client_id'), disabled: false, description: {content:"", type:"text/plain"}},
             {key: "client_secret", value: pm.environment.get('client_secret') , disabled: false, description: {content:"", type:"text/plain"}},
             {key: "grant_type", value: "refresh_token" , disabled: false, description: {content:"", type:"text/plain"}},
             {key: "refresh_token", value: pm.environment.get('refresh_token') , disabled: false, description: {content:"", type:"text/plain"}}
@@ -18,6 +18,7 @@ pm.sendRequest({
     postman.setEnvironmentVariable('access_token', res.json().access_token)
     postman.setEnvironmentVariable('refresh_token', res.json().refresh_token)
 });
+
 
 //save token to enviroment variable (first login)
 var jsonData = JSON.parse(responseBody)
